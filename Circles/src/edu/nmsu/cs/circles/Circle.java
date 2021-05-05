7b45e6db8f490fdd6ca0d20625009da57e704a39
Cameron <cameronclapp11@gmail.com>
2021-05-05 16:39:45 -0600

    Fixes #2

--- Circles/src/edu/nmsu/cs/circles/Circle.java
+++ Circles/src/edu/nmsu/cs/circles/Circle.java
@@ -1,27 +1,23 @@
 package edu.nmsu.cs.circles;
 
 /**
- * Implements a base abstract class for a circle. Everything except a intersects() method is
- * implemented here. Subclass must implement the intersects() method.
+ * Implements a base abstract class for a circle. Everything except a
+ * intersects() method is implemented here. Subclass must implement the
+ * intersects() method.
  **/
-public abstract class Circle
-{
-	protected Point		center;
+public abstract class Circle {
+	protected Point center;
 
-	protected double	radius;
+	protected double radius;
 
 	/**
 	 * Create new circle
 	 * 
-	 * @param x
-	 *          is the x coordinate of the center
-	 * @param y
-	 *          is the y coordinate of the center
-	 * @param radius
-	 *          is the radius
+	 * @param x      is the x coordinate of the center
+	 * @param y      is the y coordinate of the center
+	 * @param radius is the radius
 	 **/
-	public Circle(double x, double y, double radius)
-	{
+	public Circle(double x, double y, double radius) {
 		center = new Point();
 		center.x = x;
 		center.y = y;
@@ -31,12 +27,11 @@ public abstract class Circle
 	/**
 	 * Change size of circle
 	 * 
-	 * @param factor
-	 *          is the scaling factor (0.8 make it 80% as big, 2.0 doubles its size)
+	 * @param factor is the scaling factor (0.8 make it 80% as big, 2.0 doubles its
+	 *               size)
 	 * @return the new radius
 	 **/
-	public double scale(double factor)
-	{
+	public double scale(double factor) {
 		radius = radius + factor;
 		return radius;
 	}
@@ -44,24 +39,20 @@ public abstract class Circle
 	/**
 	 * Change position of circle relative to current position
 	 * 
-	 * @param xOffset
-	 *          is amount to change x coordinate
-	 * @param yOffset
-	 *          is amount to change y coordinate
+	 * @param xOffset is amount to change x coordinate
+	 * @param yOffset is amount to change y coordinate
 	 * @return the new center coordinate
 	 **/
-	public Point moveBy(double xOffset, double yOffset)
-	{
+	public Point moveBy(double xOffset, double yOffset) {
 		center.x = center.x + xOffset;
-		center.y = center.y + xOffset;
+		center.y = center.y + yOffset;
 		return center;
 	}
 
 	/**
 	 * Test if this circle intersects another circle.
 	 * 
-	 * @param other
-	 *          is the other circle
+	 * @param other is the other circle
 	 * @return True if the circles' outer edges intersect at all, False otherwise
 	 **/
 	public abstract boolean intersects(Circle other);
